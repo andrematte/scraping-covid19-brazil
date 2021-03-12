ac8d0abaed276bf1df45230e504dbf6ca6c2ff68
Andre Matte <carlosandrematte@gmail.com>
2021-03-11 22:59:17 -0300

    Create scraper-app.py

---
+++ app/scraper-app.py
@@ -0,0 +1,8 @@
+"""
+Brazilian Civil Registry Death Data Scraper Web App
+
+App to run web scraper functions using your browser.
+Run the app via termial by calling 'streamlit run app/scraper-app.py'.
+"""
+ 
+import streamlit as st
