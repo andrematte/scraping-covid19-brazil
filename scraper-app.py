ac0be30c1061901f13b512534c1649f6c1a05103
Andre Matte <carlosandrematte@gmail.com>
2021-03-13 22:11:06 -0300

    Finished app funcionalities

--- scraper-app.py
+++ scraper-app.py
@@ -17,7 +17,7 @@ import pandas as pd
 import random as rd
 
 from source.utils import *
-
+from config.headers import *
 
 # ----------------------------- App Configuration ---------------------------- #
 
@@ -41,15 +41,69 @@ st.image('images/sample-plot-ptrc.png')
 
  The [Brazilian Transparency Portal of Civil Registry](https://transparencia.registrocivil.org.br/especial-covid) offers data on deaths registered due to COVID-19 
  (confirmed or suspected) and respiratory diseases, such as severe acute respiratory syndrome (SARS), pneumonia, and respiratory failure. The civil registry data 
- website is based on death certificates sent by the registry offices countrywide for deaths that take place in hospitals, residences, public roads, etc [1].
+ website is based on death certificates sent by the registry offices countrywide for deaths that take place in hospitals, residences, public roads, etc.
 """
-
 
 
 # ------------------------------ Scraping Config ----------------------------- #
 
-start_date, final_date, df_selected = app_config()
-dates_2020, dates_2019, dates_2021, data_path = setup_scrape(start_date, final_date)
+start_date, final_date, df_selected, selection = app_config()
+dates_2020, dates_2019, dates_2021 = setup_dates(start_date, final_date)
 
-st.header('Selected Cities:')
-st.dataframe(df_selected)
+diseases = ['COVID', 'SRAG', 'PNEUMONIA', 'INSUFICIENCIA_RESPIRATORIA', 'SEPTICEMIA', "AVC", "CARDIOPATIA",  "CHOQUE_CARD", "COVID_AVC", "COVID_INFARTO", "INFARTO",  "SUBITA", 'INDETERMINADA', 'OUTRAS']
+save_path = f"data/PTRC-{pd.Timestamp.today().strftime('%Y-%m-%d')}-{selection}/"
+
+if st.sidebar.checkbox('Show Selected Cities'):
+    st.sidebar.header('Selected Cities:')
+    st.sidebar.dataframe(df_selected)
+    
+
+# ---------------------------- Start Web Scraping ---------------------------- #
+
+st.header('Data Collection')
+""" 
+- The web scraper settings can be found on the left sidebar. 
+- Press the button below when you're ready to start the process.
+"""
+if st.button('Start Data Collection'):
+    
+    st.write("Starting Web Scraping... This might take a while!" )
+    
+    # Create directory to save the files
+    try:
+        os.mkdir(save_path)
+    except:
+        pass
+    
+    # Scrape cities from df_selected one at a time
+    for index, row in df_selected.iterrows():
+        st.write(f"Scraping data - State: {row['Estados']} - City: {row['Cidades']}")
+        progress = st.empty()
+        
+        # Make one dataframe for each year
+        dataframe_2019 = pd.DataFrame(columns = diseases)
+        dataframe_2020 = pd.DataFrame(columns = diseases)
+        dataframe_2021 = pd.DataFrame(columns = diseases)
+        del dataframe_2019['COVID']
+        
+        for date_2020, date_2019, date_2021 in zip(dates_2020.strftime('%Y-%m-%d'), dates_2019.strftime('%Y-%m-%d'), dates_2021.strftime('%Y-%m-%d')):
+            progress.text(f'Scraping date: {date_2020}')
+            
+            # Define URL with the query
+            URL = f"https://transparencia.registrocivil.org.br/api/covid-cardiaco?start_date={date_2021}&end_date={date_2021}&state={row['Estados']}&city_id={row['ID']}&chart=chartCardiac1&places[]=HOSPITAL&places[]=DOMICILIO&places[]=VIA_PUBLICA&places[]=OUTROS&diffCity=false&cor_pele=I"
+
+            # Scrape URL and retrieve chart data
+            chart_data = scrape_url(URL, headers)
+            
+            # Process scraped data and create dataframe
+            df = process_dataframes(chart_data, diseases,
+                                    date_2019, date_2020, date_2021,
+                                    dataframe_2019, dataframe_2020, dataframe_2021)
+        
+        # Save dataframe on file_path
+        df.to_csv(save_path + f"/PTRC-{row['Estados']}-{row['Cidades']}.csv")
+        progress.text(f'Saved to: {save_path}')
+        
+    st.balloons()
+    st.success('Scraping finished!')
+        
--- source/utils.py
+++ source/utils.py
@@ -16,51 +16,52 @@ def app_config():
         start_date [datetime]: Date to start scraping from
         final_date [datetime]: Date to stop scraping
         df_selected [dataframe]: Cities selected for scraping
+        selected_cities [string]: 
     """
-    st.sidebar.title('Configurações')
-    st.sidebar.subheader('Selecione o Período')
-    st.sidebar.write('O algoritmo irá baixar os dados para o período correspondente às datas abaixo.')
-
+    st.sidebar.title('Settings')
+    
+    st.sidebar.subheader('Date Range:')
+    st.sidebar.write('Choose dates for 2020\n\n Equivalent data for 2019 and 2021 will also be downloaded.')
+    
     # Date Settings
-    start_date = st.sidebar.date_input('Data Inicial',
+    start_date = st.sidebar.date_input('Start Date',
                                     value=datetime(2020, 1, 1),
                                     min_value=datetime(2020, 1, 1),
                                     max_value=datetime(2020, 12, 31))
 
-    final_date = st.sidebar.date_input('Data Final',
+    final_date = st.sidebar.date_input('Final Date',
                                     value=datetime(2020, 12, 31),
                                     min_value=start_date,
                                     max_value=datetime(2020, 12, 31))
 
-    st.sidebar.subheader('Selecione as Cidades')
-    st.sidebar.write('Os dados referentes às cidades selecionadas serão baixados.')
-
+    
+    st.sidebar.subheader('City Select')
+    
     # City Settings
-    cities = ['Todos os estados', 'Todas as capitais', 'Selecionar estado']
-    selected_cities = st.sidebar.selectbox('Cidades', cities)
+    cities = ['All States', 'All Capitals', 'Select Individually']
+    selected_cities = st.sidebar.selectbox('Cities', cities)
                                                   
     # Process selected cities
     df_cities = pd.read_csv('config/cities.csv').sort_values(by='uf')
         
-    print(selected_cities)
-    if selected_cities == 'Todos os estados':
+    if selected_cities == 'All States':
         df_selected = pd.DataFrame(df_cities['uf'].unique(), columns=['Estados'])
         df_selected['Cidades'] = 'all'
         df_selected['ID'] = 'all'
+        selected_cities = 'STATES'
         
-        
-    elif selected_cities == 'Todas as capitais':
+    elif selected_cities == 'All Capitals':
         df_selected = df_cities[df_cities['capital'] == True][['uf', 'nome', 'city_id']]
         df_selected = df_selected.rename(columns={'uf': 'Estados', 'nome': 'Cidades', 'city_id': 'ID'})
         df_selected = df_selected.reset_index(drop=True)
-        
+        selected_cities = 'CAPITALS'
 
-    elif selected_cities == 'Selecionar estado':
+    elif selected_cities == 'Select Individually':
         selected_state = st.sidebar.selectbox('Selecione o Estado:', df_cities['uf'].unique())
         
         city_list = list(df_cities[df_cities['uf'] == selected_state ]['nome'].values)
         city_list.insert(0, 'all')
-        selected_city = st.sidebar.selectbox('Selecione a Cidade', city_list)
+        selected_city = st.sidebar.selectbox('Select City', city_list)
         
         if selected_city == 'all':
             selected_id = 'all'
@@ -69,21 +70,12 @@ def app_config():
                
         df_selected = pd.DataFrame(columns=['Estados', 'Cidades', 'ID'], index=[0])
         df_selected.iloc[0] = [selected_state, selected_city, selected_id]
-    
-    return start_date, final_date, df_selected
 
-def terminal_config():
-    """
-    Under construction
-    
-    Returns:
-        start_date [datetime]: Date to start scraping from
-        final_date [datetime]: Date to stop scraping
-        selected_cities [list]: Cities selected for scraping
-    """
-    return
+        selected_cities = f"{df_selected['Estados'].values[0]}-{df_selected['Cidades'].values[0]}"  
+        
+    return start_date, final_date, df_selected, selected_cities
 
-def setup_scrape(start_date, final_date):
+def setup_dates(start_date, final_date):
     """
     setup_scrape
 
@@ -102,202 +94,105 @@ def setup_scrape(start_date, final_date):
     dates_2020 = pd.date_range(start_date, final_date)
     dates_2019 = pd.date_range(start_date.replace(year=2019), final_date.replace(year=2019))
     dates_2021 = pd.date_range(start_date.replace(year=2021), final_date.replace(year=2021))
+        
+    return dates_2020, dates_2019, dates_2021
+
+
+# ---------------------------- Scraping Functions ---------------------------- #
+
+def scrape_url(URL, headers):
+    """
+    scrape_url 
+
+    Scrape chart data from a single chart in the webpage
+
+    Args:
+        URL (string): URL of the website containing the data query
+        headers (dictionary): Internet browser credentials
+
+    Returns:
+        dictionary: Data collected from chart
+    """
+    while True:
+        page = requests.get(URL, headers = headers)
+        if page.status_code == 200:
+            break
+        else:
+            print('Request Failed')
+            time.sleep(2 + rd.random()*5)
+
+    data_json = page.json()
+    time.sleep(0.5 + rd.random() * 3)
     
+    return data_json['chart']
+
+
+# ------------------------- Post Scraping Processing ------------------------- #
+
+def process_dataframes(chart_data, diseases, date_2019, date_2020, date_2021, dataframe_2019, dataframe_2020, dataframe_2021):
+    """
+    process_dataframes 
+
+    Build and concatenate dataframes containing all data from 2019-2021 for all the diseases.
+
+    Args:
+        chart_data (dictionary): Data collected from chart
+        diseases (list): List of disease names
+        date_2019 (datetime): Date of the current iteration
+        date_2020 (datetime): Date of the current iteration
+        date_2021 (datetime): Date of the current iteration
+        dataframe_2019 (dataframe): dataframe of the year
+        dataframe_2020 (dataframe): dataframe of the year
+        dataframe_2021 (dataframe): dataframe of the year
+
+    Returns:
+        dataframe: Dataframe containing all data
+    """
+    # Process dataframes
+    ano_2020 = pd.DataFrame(columns = diseases)
+    ano_2019 = pd.DataFrame(columns = diseases)
+    ano_2021 = pd.DataFrame(columns = diseases)
     
-    # Path to save csv files
-    data_path = f"data/PTRC_{pd.Timestamp.today().strftime('%Y-%m-%d')}/"
-    
-    return dates_2020, dates_2019, dates_2021, data_path
+    ano_2020 = ano_2020.append(chart_data['2020'], ignore_index=True)
+    ano_2019 = ano_2019.append(chart_data['2019'], ignore_index=True)
+    ano_2021 = ano_2021.append(chart_data['2021'], ignore_index=True)
 
-
-
-
-
-
-
-
-# ----------------------------- Utility Functions ---------------------------- #
-
-def scrape_data(cities, states, headers, dates, backdates, frontdates, causes, data_path):
-    '''
-        Scrapes the desired data and saves on the /data directory.
-    '''
-    
     try:
-        os.mkdir(data_path)
+        dataframe_2020.loc[f'{date_2020}'] = ano_2020.iloc[-1]
     except:
-        pass
+        dataframe_2020.loc[f'{date_2020}'] = 0
 
-    for state, city in zip(states.keys(), cities.keys()):
-
-        dataframe_2019 = pd.DataFrame(columns = causes)
-        dataframe_2020 = pd.DataFrame(columns = causes)
-        dataframe_2021 = pd.DataFrame(columns = causes)
-        del dataframe_2019['COVID']
-
-        print(f'Scraping {city}, {state} data.')
-
-        for date, backdate, frontdate in zip(dates.strftime('%Y-%m-%d'), backdates.strftime('%Y-%m-%d'), frontdates.strftime('%Y-%m-%d')):
-
-            print('Scraping the following date: ', date)
-            URL = f'https://transparencia.registrocivil.org.br/api/covid-covid-registral?start_date={date}&end_date={date}&state={state}&city_id={cities[city]}&chart=chart1&places[]=HOSPITAL&places[]=DOMICILIO&places[]=VIA_PUBLICA&places[]=OUTROS&cor_pele=I&diffCity=false     ' 
-
-            while True:
-                page = requests.get(URL, headers = headers)
-                if page.status_code == 200:
-                    break
-                else:
-                    print('Request Failed')
-                    time.sleep(2 + rd.random()*5)
-
-            data_json = page.json()
-            time.sleep(0.5 + rd.random() * 3)
-            chart = data_json['chart']
-
-            ano_2020 = pd.DataFrame(columns = causes)
-            ano_2019 = pd.DataFrame(columns = causes)
-            ano_2021 = pd.DataFrame(columns = causes)
-            
-            ano_2020 = ano_2020.append(chart['2020'], ignore_index=True)
-            ano_2019 = ano_2019.append(chart['2019'], ignore_index=True)
-            ano_2021 = ano_2021.append(chart['2021'], ignore_index=True)
-
-            try:
-                dataframe_2020.loc[f'{date}'] = ano_2020.iloc[-1]
-            except:
-                dataframe_2020.loc[f'{date}'] = 0
-
-            try:    
-                dataframe_2019.loc[f'{backdate}'] = ano_2019.iloc[-1]
-            except:
-                dataframe_2019.loc[f'{backdate}'] = 0
-                
-            try:    
-                dataframe_2021.loc[f'{frontdate}'] = ano_2021.iloc[-1]
-            except:
-                dataframe_2021.loc[f'{frontdate}'] = 0
-
-
-        dataframe_2020.fillna(0)
-        dataframe_2019.fillna(0)
-        dataframe_2021.fillna(0)
-
-        dataframe_2020['Month'] = pd.to_datetime(dataframe_2020.index)
-        dataframe_2020 = dataframe_2020.set_index('Month')
-
-        dataframe_2019['Month'] = pd.to_datetime(dataframe_2019.index)
-        dataframe_2019 = dataframe_2019.set_index('Month')
-        dataframe_2019['COVID'] = 0
+    try:    
+        dataframe_2019.loc[f'{date_2019}'] = ano_2019.iloc[-1]
+    except:
+        dataframe_2019.loc[f'{date_2019}'] = 0
         
-        dataframe_2021['Month'] = pd.to_datetime(dataframe_2021.index)
-        dataframe_2021 = dataframe_2021.set_index('Month')
-
-        concat = pd.concat([dataframe_2019, dataframe_2020, dataframe_2021], axis=0)
-        concat = concat.rename(columns={'OUTRAS': 'DEMAIS OBITOS', 'INSUFICIENCIA_RESPIRATORIA': 'INSUFICIENCIA RESPIRATORIA', 'COVID': 'COVID19'})  
-        concat.fillna(0, inplace=True)
-
-        concat.to_csv(data_path + f'/RC_{state}_{city}.csv')
-
-def scrape_data_cardiac(cities, states, headers, dates, backdates, causes, data_path):
-    '''
-        Scrapes the desired data and saves on the /data directory.
-    '''
+    try:    
+        dataframe_2021.loc[f'{date_2021}'] = ano_2021.iloc[-1]
+    except:
+        dataframe_2021.loc[f'{date_2021}'] = 0
+        
+    dataframe_2020.fillna(0)
+    dataframe_2019.fillna(0)
+    dataframe_2021.fillna(0)
     
-    try:
-        os.mkdir(data_path)
-    except:
-        pass
+    dataframe_2020['DATA'] = pd.to_datetime(dataframe_2020.index)
+    dataframe_2020 = dataframe_2020.set_index('DATA')
 
-    for state, city in zip(states.keys(), cities.keys()):
+    dataframe_2019['DATA'] = pd.to_datetime(dataframe_2019.index)
+    dataframe_2019 = dataframe_2019.set_index('DATA')
+    dataframe_2019['COVID'] = 0
+    
+    dataframe_2021['DATA'] = pd.to_datetime(dataframe_2021.index)
+    dataframe_2021 = dataframe_2021.set_index('DATA')
+    
+    concat = pd.concat([dataframe_2019, dataframe_2020, dataframe_2021], axis=0)
+    concat = concat.rename(columns={'OUTRAS': 'DEMAIS OBITOS',
+                                    'INSUFICIENCIA_RESPIRATORIA': 'INSUFICIENCIA RESPIRATORIA',
+                                    'COVID': 'COVID19',
+                                    'CHOQUE_CARD': 'CHOQUE CARDIOPATICO',
+                                    'COVID_AVC': 'AVC (COVID)',
+                                    'COVID_INFARTO': 'INFARTO (COVID)'})  
+    
+    return concat.fillna(0)
 
-        dataframe_2019 = pd.DataFrame(columns = causes)
-        dataframe_2020 = pd.DataFrame(columns = causes)
-        del dataframe_2019['COVID']
-
-        print(f'Scraping {city}, {state} data.')
-
-        for date, backdate in zip(dates.strftime('%Y-%m-%d'), backdates.strftime('%Y-%m-%d')):
-
-            print('Scraping the following date: ', date)
-            URL = f'https://transparencia.registrocivil.org.br/api/covid-cardiaco?start_date={date}&end_date={date}&state={state}&city_id={cities[city]}&chart=chartCardiac1&places[]=HOSPITAL&places[]=DOMICILIO&places[]=VIA_PUBLICA&places[]=OUTROS&diffCity=false&cor_pele=I'
-
-            while True:
-                page = requests.get(URL, headers = headers)
-                if page.status_code == 200:
-                    break
-                else:
-                    print('Request Failed')
-                    time.sleep(2 + rd.random()*5)
-
-            data_json = page.json()
-            time.sleep(0.5 + rd.random() * 3)
-            chart = data_json['chart']           
-
-            ano_2020 = pd.DataFrame(columns = causes)
-            ano_2019 = pd.DataFrame(columns = causes)
-
-            ano_2020 = ano_2020.append(chart['2020'], ignore_index=True)
-            ano_2019 = ano_2019.append(chart['2019'], ignore_index=True)
-
-            try:
-                dataframe_2020.loc[f'{date}'] = ano_2020.iloc[-1]
-            except:
-                dataframe_2020.loc[f'{date}'] = 0
-
-            try:    
-                dataframe_2019.loc[f'{backdate}'] = ano_2019.iloc[-1]
-            except:
-                dataframe_2019.loc[f'{backdate}'] = 0
-
-
-        dataframe_2020.fillna(0)
-        dataframe_2019.fillna(0)
-
-        dataframe_2020['Month'] = pd.to_datetime(dataframe_2020.index)
-        dataframe_2020 = dataframe_2020.set_index('Month')
-
-        dataframe_2019['Month'] = pd.to_datetime(dataframe_2019.index)
-        dataframe_2019 = dataframe_2019.set_index('Month')
-        dataframe_2019['COVID'] = 0
-
-        #TODO Renomear colunas com underline
-        concat = pd.concat([dataframe_2019, dataframe_2020], axis=0)
-        concat = concat.rename(columns={'OUTRAS': 'DEMAIS OBITOS', 'INSUFICIENCIA_RESPIRATORIA': 'INSUFICIENCIA RESPIRATORIA', 'COVID': 'COVID19'})  
-        concat.fillna(0, inplace=True)
-
-        concat.to_csv(data_path + f'/RC_{state}_{city}.csv')
-
-
-
-#TODO Erase Deprecated Functions        
-def load_data():
-    city_select = dict()
-    cities = dict()
-    states = dict()
-    headers = dict()
-
-    with open('../CitySelect.txt', 'r') as inf:
-        city_select.update(ast.literal_eval(inf.read()))
-
-    with open('../CityCodes.txt', 'r') as inf:
-        data_json = json.load(inf)
-
-    with open('../headers.txt', 'r') as inf:
-        headers.update(ast.literal_eval(inf.read()))
-        
-    # Avoid those city IDs (temporary fix since some cities have duplicate names)
-    drop_cities = ['3249']
-
-    city_list = [value[3:] for value in city_select.values()]
-    city_list.append('all')
-
-    for x in range(0,len(data_json)):
-        if data_json[x]['nome'] in city_list:
-
-            print(f"{data_json[x]['uf']}, {data_json[x]['nome']},  {data_json[x]['city_id']}")
-
-            if not data_json[x]['city_id'] in drop_cities:
-                states[data_json[x]['uf']] = data_json[x]['nome']
-                cities[data_json[x]['nome']] = data_json[x]['city_id']
-        
-    return city_select, cities, states, headers
