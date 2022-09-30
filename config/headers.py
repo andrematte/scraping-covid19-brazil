425d469095dd34f42b3379422137944942189a67
André Mattos <carlosandrematte@gmail.com>
2022-09-30 17:31:30 -0300

    Automated User Agent

--- config/headers.py
+++ config/headers.py
@@ -1,4 +1,9 @@
-headers = {'User-Agent': '',
+import pyuser_agent
+
+pua = pyuser_agent.UA()
+
+
+headers = {'User-Agent': pua.mozilla,
            'X-CSRF-TOKEN': '',
            'X-XSRF-TOKEN': ''
            }
